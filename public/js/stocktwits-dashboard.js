f289c438d1f8fd966f29779adc38b7d9e01bc3e4
Yuxiang Zhang <rico.zhang9318@gmail.com>
2016-04-23 19:20:38 -0400

    Add search input interaction

--- public/js/stocktwits-dashboard.js
+++ public/js/stocktwits-dashboard.js
@@ -1,16 +1,17 @@
 // StockTwits Realtime Dashboard
-var sector = "All";
-var sectorAPI = "https://stocktwitsbackend.herokuapp.com/sectors";
-var symbolAPI = "https://stocktwitsbackend.herokuapp.com/search";
-var symbolInfoField = ["prev_close", "open", "volume", 'pe', 'eps'];
-var symbolInfoId = ["symbol-pc", "symbol-op","symbol-vo","symbol-pe","symbol-eps"];
+let sector = "All";
+let sectorAPI = "https://stocktwitsbackend.herokuapp.com/sectors";
+let symbolAPI = "https://stocktwitsbackend.herokuapp.com/search";
+let symbolInfoField = ["prev_close", "open", "volume", 'pe', 'eps'];
+let symbolInfoId = ["symbol-pc", "symbol-op", "symbol-vo", "symbol-pe", "symbol-eps"];
 
+// Get API /sectors?sector= and append symbols within that sector to the left 
 function symbolListSector(sector) {
     $.getJSON(sectorAPI, {
             sector: sector
         })
         .done(function(data) {
-            var t = $("#table1").DataTable();
+            let t = $("#overview").DataTable();
             $.each(data[Object.keys(data)], function(i, item) {
                 t.row.add([
                     '<a class="symbol_a" value=' + item.name + '>' + item.name + '</a>',
@@ -27,10 +28,10 @@ function symbolInfo(symbol) {
             symbol: symbol
         })
         .done(function(data) {
-            var hdata = data['data'][0]['data'];
-            var record = hdata[hdata.length - 1];
-            $.each(symbolInfoField,function(i,item){
-                document.getElementById(symbolInfoId[i]).innerHTML=record[item]
+            let hdata = data['data'][0]['data'];
+            let record = hdata[hdata.length - 1];
+            $.each(symbolInfoField, function(i, item) {
+                document.getElementById(symbolInfoId[i]).innerHTML = record[item]
             })
         })
 }
@@ -45,10 +46,10 @@ $(document).ready(function() {
     // Handle panel toggle
     $('.panel-toggle').on('click', function(event) {
         event.preventDefault();
-        var hpanel = $(event.target).closest('div.panel');
-        var icon = $(event.target).closest('i');
-        var body = hpanel.find('div.panel-body');
-        var footer = hpanel.find('div.panel-footer');
+        let hpanel = $(event.target).closest('div.panel');
+        let icon = $(event.target).closest('i');
+        let body = hpanel.find('div.panel-body');
+        let footer = hpanel.find('div.panel-footer');
         body.slideToggle(300);
         footer.slideToggle(200);
 
@@ -64,7 +65,7 @@ $(document).ready(function() {
     // Handle panel close
     $('.panel-close').on('click', function(event) {
         event.preventDefault();
-        var hpanel = $(event.target).closest('div.panel');
+        let hpanel = $(event.target).closest('div.panel');
         hpanel.remove();
     });
 
@@ -73,42 +74,60 @@ $(document).ready(function() {
         $('.btn-filter').not(this).removeClass('active'); // remove buttonactive from the others
         $(this).toggleClass('active')
     });
-    // Handle Symbol Search !!!!!!!!!!!!!To be done
-    $("#symbol-search").bind('keyup', function(event) {
-        event.preventDefault();
-        if (event.keyCode == 13) {
-            console.log('yes');
+
+    // Handle Symbol Search 
+    $("#symbol-search").keypress(function(event) {
+        let keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '13') {
+            // get input value
+            let val = $(this).val().toUpperCase();
+            document.getElementById("symbol-header").innerHTML = val;
+            symbolInfo(val);
+            document.getElementById("symbol-search").value = ""
         }
     });
-
+    let availableTags = ["AAPL", "ABBV", "ABT", "ACN", "AIG", "ALL", "AMGN", "AMZN", "APA", "AXP", "BA", "BAC", "BAX", "BIIB", "BK", "BLK", "BMY", "BRKB", "C", "CAT", "CL", "CMCSA",
+        "COF", "COP", "COST", "CSCO", "CVS", "CVX", "DD", "DIS", "DOW", "DVN", "EBAY", "EMC", "EMR", "EXC", "F", "FB", "FCX", "FDX", "FOXA", "GD", "GE", "GILD",
+        "GM", "GOOG", "GS", "HAL", "HD", "HON", "IBM", "INTC", "JNJ", "JPM", "KO", "LLY", "LMT", "LOW", "MA", "MCD", "MDLZ", "MDT", "MET", "MMM", "MO", "MON", "MRK", "MS", "MSFT", "NEE", "NKE", "NOV", "NSC", "ORCL", "OXY", "PEP", "PFE", "PG", "PM", "QCOM", "RTN", "SBUX", "SLB", "SO", "SPG", "T", "TGT", "TWX", "TXN", "UNH", "UNP", "UPS", "USB", "UTX", "V", "VZ", "WBA", "WFC", "WMT", "XOM"
+    ]
+    $("#symbol-search").autocomplete({
+        source: availableTags,
+        response: function(event, ui) {
+            // ui.content is the array that's about to be sent to the response callback.
+            if (ui.content.length === 0) {
+                $("#symbol-search").text("No results found");
+            } else {
+                $("#symbol-search").empty();
+            }
+        }
+    });
     // Btn filter click and Refresh and append the symbol list 
-    var sector = "All";
+    let sector = "All";
     symbolListSector(sector);
 
     $('.btn-filter').click(function() {
         sector = $(this).attr('value');
-        $("#table1").DataTable().clear();
+        $("#overview").DataTable().clear();
         symbolListSector(sector);
     });
-    // DataTable setting up
-    var table1 = $('#table1').DataTable({
+    // overview setting up
+    let overview = $('#overview').DataTable({
         "order": [
             [1, "desc"]
         ],
         "searching": false,
         "paging": false,
-
         "lengthChange": false,
         "info": false
     });
-    // DataTable click and refresh symbol-info information
-    // Onload
-    var symbol_name = 'AAPL';
+    // "overview": click on each symbol and refresh symbol-info information
+    //// Onload
+    let symbol_name = 'AAPL';
     symbolInfo(symbol_name);
-    // Onclick 
-    $('#table1 tbody').on('click', 'td', function() {
-        var cell = table1.cell(this).node();
-        var value = cell.childNodes[0];
+    //// Onclick symbol
+    $('#overview tbody').on('click', 'td', function() {
+        let cell = overview.cell(this).node();
+        let value = cell.childNodes[0];
         symbol_name = value.getAttribute("value");
         document.getElementById("symbol-header").innerHTML = symbol_name;
         symbolInfo(symbol_name);
